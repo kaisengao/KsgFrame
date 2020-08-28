3635999662244e9f096c0f6942097f72493a958c
kaisengao <kaisengao28@163.com>
2020-08-28 15:29:29 +0800

    1、修复一下Toolbar的bug

---
+++ lib_mvvm/src/main/java/com/kaisengao/mvvm/binding/adapter/view/ViewAdapter.java
@@ -0,0 +1,30 @@
+package com.kaisengao.mvvm.binding.adapter.view;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.databinding.BindingAdapter;
+
+import com.kasiengao.base.BaseApplication;
+import com.kasiengao.base.util.StatusBarUtil;
+
+/**
+ * @ClassName: ViewAdapter
+ * @Author: KaiSenGao
+ * @CreateDate: 2020/6/28 11:36
+ * @Description:
+ */
+public class ViewAdapter {
+
+    @BindingAdapter("setViewHeight")
+    public static void setViewHeight(View view, int height) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = height;
+        view.requestLayout();
+    }
+
+    @BindingAdapter("setPaddingSmart")
+    public static void setPaddingSmart(View view, int padding) {
+        StatusBarUtil.setPaddingSmart(BaseApplication.getInstance(), view);
+    }
+}
